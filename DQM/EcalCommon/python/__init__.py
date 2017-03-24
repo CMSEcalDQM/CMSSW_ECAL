6221b89edb48f0a7c7dd90f9415d910729d12e71
Giacomo Cucciati <giacomo.cucciati@cern.ch>
2017-03-24 16:39:20 +0100

    Explicitly allow destructor to throw exceptions, needed while moving to gcc530 from gcc493

--- DQM/EcalCommon/python/__init__.py
+++ DQM/EcalCommon/python/__init__.py
@@ -1,3 +1,3 @@
 #Automatically created by SCRAM
 import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/DQM/EcalCommon/',1)[0])+'/cfipython/slc6_amd64_gcc481/DQM/EcalCommon')
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/DQM/EcalCommon/',1)[0])+'/cfipython/slc6_amd64_gcc530/DQM/EcalCommon')
--- DQMOffline/Ecal/python/__init__.py
+++ DQMOffline/Ecal/python/__init__.py
@@ -1,3 +1,3 @@
 #Automatically created by SCRAM
 import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/DQMOffline/Ecal/',1)[0])+'/cfipython/slc6_amd64_gcc481/DQMOffline/Ecal')
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/DQMOffline/Ecal/',1)[0])+'/cfipython/slc6_amd64_gcc530/DQMOffline/Ecal')
